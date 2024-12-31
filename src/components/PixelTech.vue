ca3b1d1147945d1822434ae8b801d700c7e0acd2
ahsanabdullah-1212 <154989073+ahsanabdullah-1212@users.noreply.github.com>
2024-12-31 19:55:51 +0500

    3d-model changes

--- src/components/PixelTech.vue
+++ src/components/PixelTech.vue
@@ -26,14 +26,14 @@
                         </router-link>
                     </div>
                 </div>
-               
+
                 <div class="pixel-col-2" style="position:relative">
-                    <div class="pixel-image">
+                    <div class="pixel-image" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
                         <div class="model-viewer" ref="modelViewer"></div>
                     </div>
                 </div>
-                <div class="modelloader pixel-col-2" v-if="modelLoader" >
-                    <div class="loader-component"  >
+                <div class="modelloader pixel-col-2" v-if="modelLoader">
+                    <div class="loader-component">
                         <LoaderComponent />
                     </div>
                 </div>
@@ -41,6 +41,7 @@
         </div>
     </div>
 </template>
+
 <script>
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -54,6 +55,7 @@ export default {
     data() {
         return {
             modelLoader: true,
+            isMouseInside: false,  // Track if the mouse is inside the div
         }
     },
     mounted() {
@@ -66,11 +68,11 @@ export default {
         init() {
             const container = this.$refs.modelViewer;
             this.scene = new THREE.Scene();
-            this.camera = new THREE.PerspectiveCamera(100, container.clientWidth / 400, 0.1, 1000);
-            this.camera.position.set(0, 4, 3.85);
+            this.camera = new THREE.PerspectiveCamera(70, container.clientWidth / 400, 0.1, 1000);
+            this.camera.position.set(0, 3, 6);
 
             this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-            this.renderer.setSize(container.clientWidth, 400); 
+            this.renderer.setSize(container.clientWidth, 400);
             this.renderer.shadowMap.enabled = true;
             this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
@@ -82,61 +84,22 @@ export default {
             window.addEventListener('resize', this.onWindowResize);
         },
 
-        setupLights() {
-            const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1.5);
-            directionalLight1.position.set(5, 10, 5);
-            directionalLight1.castShadow = true;
-            this.scene.add(directionalLight1);
-
-            const directionalLight2 = new THREE.DirectionalLight(0xf4a7a7, 1.5);
-            directionalLight2.position.set(-5, 3, 0);
-            directionalLight2.castShadow = true;
-            this.scene.add(directionalLight2);
-
-            const rimLight = new THREE.DirectionalLight(0x005d79, 1.5);
-            rimLight.position.set(10, 25, 0);
-            this.scene.add(rimLight);
-
-            const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-            this.scene.add(ambientLight);
+        onMouseEnter() {
+            this.isMouseInside = true;  // Mouse entered the div
         },
 
-        loadModel() {
-            const loader = new GLTFLoader();
-            loader.load('pixieyes10.glb', (gltf) => {
-                this.character = gltf.scene;
-
-                this.character.traverse((node) => {
-                    if (node.isMesh) {
-                        node.castShadow = true;
-                        node.receiveShadow = true;
-                        node.material.needsUpdate = true;
-                    }
-                });
-
-                this.headBone = this.character.getObjectByName('head');
-                this.spine2Bone = this.character.getObjectByName('spine2');
-                this.spineBone = this.character.getObjectByName('spine');
-                this.eyerBone = this.character.getObjectByName('eyeR');
-                this.eyelBone = this.character.getObjectByName('eyeL');
-
-                this.scene.add(this.character);
-                this.mixer = new THREE.AnimationMixer(this.character);
-                const action = this.mixer.clipAction(gltf.animations[0]);
-                action.timeScale = 0.4;
-                action.play();
-                this.modelLoader = false;
-            }, undefined, (error) => {
-                console.error('An error occurred while loading the model:', error);
-                this.modelLoader = false;
-            });
+        onMouseLeave() {
+            this.isMouseInside = false;  // Mouse left the div
         },
 
         onMouseMove(event) {
-            const container = this.$refs.modelViewer;
+            if (!this.isMouseInside) return;  // Only process if the mouse is inside the div
 
-            const mouseX = (event.clientX / container.clientWidth) * 2 - 1;
-            const mouseY = -(event.clientY / 400) * 2 + 0.55;
+            const container = this.$refs.modelViewer;
+            const rect = container.getBoundingClientRect();
+
+            const mouseX = (event.clientX - rect.left) / rect.width * 2 - 1;
+            const mouseY = -(event.clientY - rect.top) / rect.height * 2 + 0.55;
 
             if (this.headBone) {
                 this.headBone.rotation.y = mouseX * 0.5;
@@ -167,11 +130,59 @@ export default {
         },
 
         onWindowResize() {
-            const container = this.$refs.modelViewer; 
-
+            const container = this.$refs.modelViewer;
             this.camera.aspect = container.clientWidth / 400;
             this.camera.updateProjectionMatrix();
             this.renderer.setSize(container.clientWidth, 400);
+        },
+
+        setupLights() {
+            const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1.5);
+            directionalLight1.position.set(5, 10, 5);
+            directionalLight1.castShadow = true;
+            this.scene.add(directionalLight1);
+
+            const directionalLight2 = new THREE.DirectionalLight(0xf4a7a7, 1.5);
+            directionalLight2.position.set(-5, 3, 0);
+            directionalLight2.castShadow = true;
+            this.scene.add(directionalLight2);
+
+            const rimLight = new THREE.DirectionalLight(0x005d79, 1.5);
+            rimLight.position.set(10, 25, 0);
+            this.scene.add(rimLight);
+
+            const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+            this.scene.add(ambientLight);
+        },
+
+        loadModel() {
+            const loader = new GLTFLoader();
+            loader.load('pixieyes10.glb', (gltf) => {
+                this.character = gltf.scene;
+                this.character.traverse((node) => {
+                    if (node.isMesh) {
+                        node.castShadow = true;
+                        node.receiveShadow = true;
+                        node.material.needsUpdate = true;
+                    }
+                });
+
+                this.headBone = this.character.getObjectByName('head');
+                this.spine2Bone = this.character.getObjectByName('spine2');
+                this.spineBone = this.character.getObjectByName('spine');
+                this.eyerBone = this.character.getObjectByName('eyeR');
+                this.eyelBone = this.character.getObjectByName('eyeL');
+
+                this.scene.add(this.character);
+                this.mixer = new THREE.AnimationMixer(this.character);
+                const action = this.mixer.clipAction(gltf.animations[0]);
+                action.timeScale = 0.4;
+                action.play();
+                this.modelLoader = false;
+            }, undefined, (error) => {
+                console.error('An error occurred while loading the model:', error);
+                this.modelLoader = false;
+            });
         },
 
         cleanup() {
@@ -182,10 +193,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-.model-viewer {
-    width: 100%;
-    height: auto;
-}
-</style>
